75db06ecd6026dcaa1fb1889c2dc83215ee152bb
Đinh Đức Nhật <54789373+trangnha89@users.noreply.github.com>
2019-09-02 11:40:27 +0700

    creat web.php

---
+++ web.php
@@ -0,0 +1,348 @@
+
+<!DOCTYPE html>
+<html lang="vi" prefix="og: http://ogp.me/ns#">
+<head>
+  <meta charset="utf-8">
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:site" content="@chatwork_en">
+  <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W4MK3X');</script>
+<!-- End Google Tag Manager -->
+
+  <title>Chatwork | Chat nhóm xuyên quốc gia</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="Chatwork là ứng dụng nhắn tin, gọi điện được thiết kế dành riêng cho các doanh nghiệp, tích hợp đầy đủ các tính năng vượt trội giúp quản lý công việc và tập tin một cách dễ dàng và hiệu quả.">
+  <meta name="keywords" content="Chatwork vietnam, su dung Chatwork, cai dat Chatwork, download Chatwork, Voice chat, chat nhom, quan ly cong viec, quan ly du an, chia se tap tin">
+  <meta name="apple-itunes-app" content="app-id=463672966" />
+  <meta name="google-play-app" content="app-id=jp.ecstudio.chatworkandroid">
+  <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=yes">
+  <meta name="format-detection" content="telephone=no">
+  <link rel="alternate" href="https://go.chatwork.com/" hreflang="en"  />
+<link rel="alternate" href="https://go.chatwork.com/ja/index.html" hreflang="ja"  />
+<link rel="alternate" href="https://go.chatwork.com/vi/" hreflang="vi"  />
+<link rel="alternate" href="https://go.chatwork.com/zh-tw/" hreflang="zh-tw"  />
+<link rel="alternate" href="https://go.chatwork.com/th/" hreflang="th"  />
+<link rel="alternate" href="https://go.chatwork.com/es/" hreflang="es"  />
+<link rel="alternate" href="https://go.chatwork.com/" hreflang="x-default"  />
+  <link rel="alternate" hreflang="vi" href="https://go.chatwork.com/vi/index.html" />
+  <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+  <link href="./image/common/touch-icon-iphone.png" rel="apple-touch-icon-precomposed">
+  <link href="./image/common/touch-icon-ipad.png" rel="apple-touch-icon-precomposed" sizes="76x76">
+  <link href="./image/common/touch-icon-iphone-retina.png" rel="apple-touch-icon-precomposed" sizes="120x120">
+  <link href="./image/common/touch-icon-ipad-retina.png" rel="apple-touch-icon-precomposed" sizes="152x152">
+  <link href="./style/base.min.css" rel="stylesheet" media="screen,tv">
+  
+  <link href="./image/common/favicon.ico" rel="shortcut icon">
+  <!--[if lt IE 9]>
+  <script src="//cdn.chatwork.com/util/html5/v3.7.0/html5.js"></script>
+  <script>window.html5 || document.write('<script src="//s3-ap-northeast-1.amazonaws.com/cdn.chatwork.com/util/html5/v3.7.0/html5.js"><\/script>')</script>
+  <![endif]-->
+  
+  <script src="//use.typekit.net/clg2qao.js"></script>
+  <script>try{Typekit.load();}catch(e){}</script>
+  
+  <script src="./javascript/lib/jquery.js"></script>
+  <script src="./javascript/lib/underscore.js"></script>
+  <script src="./javascript/lib/hogan-3.0.1.js"></script>
+  <meta property="og:title" content="Chatwork">
+<meta property="og:image" content="https://go.chatwork.com/vi/image/common/OGP.png?v=140808">
+<meta property="og:description" content="Chatwork is a communication platform designed for companies and teams, also comes with fully integrated task and file management feature.">
+<meta property="og:type" content="website">
+<meta property="og:site_name" content="Chatwork">
+<meta property="og:url" content="https://go.chatwork.com/vi/index.html">
+<meta property="og:locale" content="en_US">
+<meta property="fb:admins" content="100000391675297">
+</head>
+<!--[if lte IE 8]>
+<body class="ie8">
+<![endif]-->
+<!--[if gte IE 9]>
+<body>
+<![endif]-->
+<!--[if !IE]><!-->
+<body>
+<!--<![endif]-->
+  <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W4MK3X"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+  <section class="section--header">
+  <header class="header">
+    <div class="header__nav-btn" id="_draw-handler">
+      <i class="iconfont icon_sp_menu"></i>
+    </div>
+    <h1 class="header__logo">
+      <a href="./">
+        <img src="./image/common/logo_hz.png" alt="Chatwork">
+      </a>
+    </h1>
+    <nav class="header__nav" id="_drawer">
+      <ul class="global-nav">
+        <li class="global-nav__item">
+          <a href="./product/">Sản phẩm</a>
+        </li>
+        <li class="global-nav__item">
+          <a href="./pricing/">Bảng giá</a>
+        </li>
+        <li class="global-nav__item">
+          <a href="./case/">Trường hợp thực tế</a>
+        </li>
+        <li class="global-nav__item">
+          <a href="./security/">Bảo mật</a>
+        </li>
+        <li class="global-nav__item">
+          <a href="./download/">Tải về</a>
+        </li>
+        <li class="global-nav__item">
+          <a href="http://help.chatwork.com/hc/vi">Hỏi đáp</a>
+        </li>
+        <li class="global-nav__item  btn-signin">
+          <a href="https://www.chatwork.com/?login=vi" class="btn  btn--wire">Đăng nhập</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</section>
+
+  <section class="section section--hero home-hero">
+  <div class="section__inner home-hero__inner">
+    <div class="home-hero__text">
+      <h1 class="heading heading--hero hero-title">
+        <span>Năng suất tăng 10 lần, Doanh thu tăng 20%</span></h1>
+    </div>
+    <img src="./image/index/img_hero_device.png" alt="" class="home-hero__img">
+  </div>
+  <div class="hero-signup">
+    <div class="hero-signup__inner">
+      <div class="hero-signup__box hero-cta__signup--mail">
+        
+        <form class="closing__signup signup _funnel-analysis" method="post" action="https://www.chatwork.com/service/packages/chatwork/pre_register.php?plan=free&lang=vi&social_off=1">
+          <div class="signup__form">
+            <label class="signup-label" for="input-mail">Đăng ký miễn phí</label>
+            <input name="email" type="email" id="input-mail" placeholder="Nhập email của bạn" class="signup-input">
+            <!--[if lte IE 9]><p class="signup__form__placeholder">Nhập email của bạn</p><![endif]-->
+          </div>
+          <div class="signup__button  button-container">
+            <button name="check" type="submit" class="btn  is_register btn--hero-mail">Bắt đầu</button>
+          </div>
+        </form>
+      </div>
+      <div class="hero-signup__box hero-cta__signup--document">
+        <a href="https://mpages.chatwork.com/CW-VN-form_standard.html" class="btn btn--hero-document _funnel-analysis" target="_blank">Tải tài liệu</a>
+      </div>
+      
+    </div>
+  </div>
+</section>
+<section class="section section--what section--plane home-what">
+  <div class="section__inner">
+    <div class="home-what__text">
+      <h2 class="heading heading--what"><span class="transform-none">Chatwork </span>là gì?</h2>
+      <p>Chatwork là một nền tảng tích hợp tất cả trong một: tính năng chat, chỉ định công việc, gọi thoại/video và chia sẻ tập tin.</p>
+      <p>Chatwork giúp nhân viên dễ dàng làm việc theo nhóm bằng hình thức đối thoại trực tiếp, tăng khả năng tương tác và nâng cao hiệu quả công việc.</p>
+      <p>Hơn <span>225,000</span> công ty, khu vực sử dụng Chatwork trao đổi thông tin, nâng cao hiệu suất và đối thoại để thành công.</p>
+    </div>
+    <img src="./image/index/img_tablet.png" alt="" class="home-what__img">
+  </div>
+</section>
+<section class="section section--feature home-feature ">
+  <div class="section__inner">
+    <div class="home-feature__head">
+      <h2 class="heading heading--square"><span>Tính năng vượt trội</span></h2>
+    </div>
+  </div>
+  <section class="home-feature__item">
+    <div class="feature-pop">
+      <div class="feature-pop__inner">
+        <h3 class="heading feature-pop__head feature-pop__head--group">trò chuyện nhóm</h3>
+        <ul class="feature-check-list">
+          <li>Phòng trò chuyện tạo riêng cho từng nhóm hoặc từng dự án với nhiều thành viên tham gia.</li>
+          <li>Có thể gửi tin nhắn bất kể đối phương đang trực tuyến hay ngoại tuyến.</li>
+        </ul>
+      </div>
+    </div>
+    <div class="feature-img feature-img--group">
+      <div class="feature-img__inner">
+        <span class="animation-img animation-img--group"><img src="./image/index/groupchat.gif" alt=""></span>
+        <img src="./image/index/img_feature_group.png" alt="">
+      </div>
+    </div>
+  </section>
+  <section class="home-feature__item">
+    <div class="feature-pop">
+      <div class="feature-pop__inner">
+        <h3 class="heading feature-pop__head feature-pop__head--task">quản lý công việc</h3>
+        <ul class="feature-check-list">
+          <li>Truyền tải công việc, chỉ định người thực hiện, quy định thời hạn công việc, tránh việc bỏ sót tin nhắn.</li>
+          <li>Tin nhắn tự động gửi đến tất cả thành viên trong nhóm khi một thành viên hoàn thành nhiệm vụ.</li>
+        </ul>
+      </div>
+    </div>
+    <div class="feature-img feature-img--task">
+      <div class="feature-img__inner">
+        <span class="animation-img animation-img--task"><img src="./image/index/task.gif" alt=""></span>
+        <img src="./image/index/img_feature_task.png" alt="">
+      </div>
+    </div>
+  </section>
+  <section class="home-feature__item">
+    <div class="feature-pop">
+      <div class="feature-pop__inner">
+        <h3 class="heading feature-pop__head feature-pop__head--file">chia sẻ tập tin</h3>
+        <ul class="feature-check-list">
+          <li>Chia sẻ tập tin bất kể định dạng nào.</li>
+          <li>Tất cả các tài liệu được sắp xếp ngăn nắp gọn gàng theo từng dự án, từng chủ đề, từng bối cảnh.</li>
+          <li>Lưu trữ trên đám mây dữ liệu với dung lượng lên đến 10Gb.</li>
+        </ul>
+      </div>
+    </div>
+    <div class="feature-img feature-img--file">
+      <div class="feature-img__inner">
+        <span class="animation-img animation-img--file"><img src="./image/index/file.gif" alt=""></span>
+        <img src="./image/index/img_feature_file.png" alt="">
+      </div>
+    </div>
+  </section>
+  <section class="home-feature__item">
+    <div class="feature-pop">
+      <div class="feature-pop__inner">
+        <h3 class="heading feature-pop__head feature-pop__head--video">Gọi thoại / video</h3>
+        <ul class="feature-check-list">
+          <li>Có thể thực hiện gọi video với tối đa 14 thành viên trong nhóm và gọi thoại với tối đa 100 thành viên trong nhóm.</li>
+          <li>Có thể chia sẻ màn hình khi đang gọi video/thoại.</li>
+          <li>Có thể thực hiện cuộc gọi cả trên máy tính và điện thoại.</li>
+        </ul>
+      </div>
+    </div>
+    <div class="feature-img feature-img--video">
+      <div class="feature-img__inner">
+        <span class="animation-img animation-img--video"><img src="./image/index/groupcall.gif" alt=""></span>
+        <img src="./image/index/img_feature_video.png" alt="">
+      </div>
+    </div>
+  </section>
+</section>
+<section class="section section--plane section--why home-why">
+  <div class="section__inner home-why__inner">
+    <div class="why-text">
+      <h2 class="heading heading--why">Tại sao nên chọn <span class="transform-none">Chatwork </span></h2>
+      <ul class="why-reason-list">
+        <li><span class="why-reason-list__number">1</span><span class="why-reason-list__phrase">Thời gian các buổi họp được rút ngắn 1/3, tiết kiệm trung bình 1,26h làm việc</span></li>
+        <li><span class="why-reason-list__number">2</span><span class="why-reason-list__phrase">Hạn chế việc nhân viên sử dụng để chat với mục đích cá nhân, không tập trung vào công việc</span></li>
+        <li><span class="why-reason-list__number">3</span><span class="why-reason-list__phrase">An tâm gửi dữ liệu quan trọng khi được bảo mật tuyệt đối</span></li>
+        <li><span class="why-reason-list__number">4</span><span class="why-reason-list__phrase">Tần suất sử dụng điện thoại giảm chỉ còn 70% so với trước đây, những nhầm lẫn trong trao đổi cũng được khắc phục hoàn toàn.</span></li>
+        <li><span class="why-reason-list__number">5</span><span class="why-reason-list__phrase">Giao diện đơn giản dễ sử dụng</span></li>
+        <li><span class="why-reason-list__number">6</span><span class="why-reason-list__phrase">Truy cập mọi lúc mọi nơi với ứng dụng dành riêng cho iOS và Android</span></li>
+        <li><span class="why-reason-list__number">7</span><span class="why-reason-list__phrase">Tiện lợi với ứng dụng dành riêng cho iOS và Android</span></li>
+      </ul>
+    </div>
+  </div>
+</section>
+
+    
+<section class="cta cta--footer">
+  <div class="section__inner">
+    <div class="cta--footer__inner">
+      <div class="cta-box cta-box--mail">
+        <h2 class="heading  cta__catch">Đăng ký miễn phí</h2>
+        
+        <div class="cta__mail align-center">
+          <form class="signup _funnel-analysis" method="post" action="https://www.chatwork.com/service/packages/chatwork/pre_register.php?plan=free&lang=vi&social_off=1">
+            <div class="signup__form">
+              <input class="input input--signup" name="email" type="email" id="input-mail" placeholder="Nhập email của bạn">
+              <!--[if lte IE 9]><p class="signup__form__placeholder">Nhập email của bạn</p><![endif]-->
+            </div>
+            <div class="signup__button">
+              <button class="btn btn--mail" name="check" type="submit">Bắt đầu</button>
+            </div>
+          </form>
+        </div>
+        
+      </div>
+      <div class="cta-box cta-box--document">
+        <a href="https://mpages.chatwork.com/CW-VN-form_standard.html" target="_blank" class="btn btn--document _funnel-analysis">Tải tài liệu</a>
+      </div>
+    </div>
+
+    <div class="cta__distributor">
+      <h2 class="ttl-distributor">Trở thành đối tác kinh doanh</h2>
+      <p>Nếu bạn quan tâm đến việc trở thành đối tác bán hàng của Chatwork, hãy tìm hiểu chương trình và liên hệ ngay với chúng tôi.</p>
+      <a href="https://mpages.chatwork.com/CW-VN-form_partner.html" class="_funnel-analysis" target="_blank"><span class="iconfont icon_arrow_circle_right"></span> Bấm vào đây để biết về chương trình đối tác kinh doanh</a>
+    </div>
+  </div>
+</section>
+
+
+      <div class="section--footer">
+  <footer class="footer">
+    
+    <p class="footer__brand">
+      <a href="./">
+        <img src="./image/common/logo_hz_bl.png" alt="Chatwork">
+      </a>
+    </p>
+    
+    
+    <div class="grid-container">
+      <ul class="footer__nav  grid-column">
+        <li><a href="./product/">Sản phẩm</a></li>
+        <li><a href="./pricing/">Bảng giá</a></li>
+        <li><a href="./security/">Bảo mật</a></li>
+        <li><a href="./case/">Trường hợp thực tế</a></li>
+        <li><a href="./download/">Tải về</a></li>
+        <li><a href="http://help.chatwork.com/hc/vi">Hỏi đáp</a></li>
+        <li><a href="http://developer.chatwork.com/vi/">API</a></li>
+        <li><a href="./company/">Công ty</a></li>
+        <li><a href="./terms.html">Điều khoản sử dụng</a></li>
+        <li><a href="./privacy.html">Quyền riêng tư</a></li>
+        <li><a href="https://go.chatwork.com/blog/en/">Nhật ký điện tử</a></li>
+      </ul>
+      <div class="footer__language">
+         <button tabindex="0" aria-haspopup="true" aria-controls="language-list" title="Select region" class="select-language _language-trigger">Tiếng Việt</button>
+        <ul role="menu" id="language-list" class="language-name _language-view">
+          <li lang="ja"><a href="https://go.chatwork.com/ja/">日本語</a></li>
+          <li lang="en"><a href="https://go.chatwork.com/">English</a></li>
+          <li lang="es"><a href="https://go.chatwork.com/es/">Español</a></li>
+          <li lang="zh-cmn-Hant-TW"><a href="https://go.chatwork.com/zh-tw/">繁體中文</a></li>
+          <li lang="vi"><a href="https://go.chatwork.com/vi/">Tiếng Việt</a></li>
+          <li lang="th"><a href="https://go.chatwork.com/th/">ภาษาไทย</a></li>
+        </ul>
+
+      </div>
+    </div>
+    
+    
+    <ul class="footer__social  social">
+      <li class="social__btn">
+        <a href="https://www.facebook.com/chatwork">
+          <i class="iconfont  icon_facebook"></i>
+        </a>
+      </li>
+      <li class="social__btn">
+        <a href="https://twitter.com/chatwork_en/">
+          <i class="iconfont  icon_twitter"></i>
+        </a>
+      </li>
+      <li class="social__btn">
+        <a href="http://www.linkedin.com/company/chatwork">
+          <i class="iconfont  icon_linkedin"></i>
+        </a>
+      </li>
+    </ul>
+    
+    <small class="copyright  small-text">
+      &copy; Chatwork
+    </small>
+  </footer>
+</div>
+
+    
+  <script src="./javascript/chatwork.min.js"></script>
+  
+</body>
+</html>
